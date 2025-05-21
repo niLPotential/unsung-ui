db13e06482b3a569809e66f2c420e4c544f77ccd
niLPotential <niLPotential@proton.me>
2025-05-22 03:55:59 +0900

    Add preset mini

--- components/react/uno.config.ts
+++ components/react/uno.config.ts
@@ -1,3 +1,5 @@
-import { defineConfig } from "unocss";
+import { defineConfig, presetMini } from "unocss";
 
-export default defineConfig({});
+export default defineConfig({
+  presets: [presetMini()],
+});
